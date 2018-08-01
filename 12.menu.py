9bb7b9643f7cb95ad496ac099f406dfe98afc453
HardPlant <abc7988se@naver.com>
2018-08-01 22:34:36 +0900

    add a menu

---
+++ 12.menu.py
@@ -0,0 +1,36 @@
+'''
+menu
+
+'''
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp
+from PyQt5.QtGui import QIcon
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+    
+    def initUI(self):
+
+        exitAct = QAction(QIcon('exit.png'), '&Exit', self) # Abstraction for actions, exit label
+        exitAct.setShortcut('Ctrl+Q') # shortcut
+        exitAct.setStatusTip('Exit Application') # modify statusbar
+        exitAct.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('')
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
