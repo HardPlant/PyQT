253660c63da41db833fc3934d7250e7bc0098bf8
HardPlant <abc7988se@naver.com>
2018-08-01 22:47:32 +0900

    context menu

---
+++ 15.popup.py
@@ -0,0 +1,33 @@
+'''
+context menu(popup)
+'''
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMenu, qApp
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+    
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('')
+        self.show()
+
+    def contextMenuEvent(self, event):
+        cmenu = QMenu(self)
+
+        newAct = cmenu.addAction("New")
+        opnAct = cmenu.addAction("Open")
+        quitAct = cmenu.addAction("Quit")
+        action = cmenu.exec_(self.mapToGlobal(event.pos()))
+
+        if action==quitAct:
+            qApp=quit()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
