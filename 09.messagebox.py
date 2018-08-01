ff38bbc3d6ed4d87cd33476513300794aeec6eb3
HardPlant <abc7988se@naver.com>
2018-08-01 22:08:37 +0900

    messagebox

---
+++ 09.messagebox.py
@@ -0,0 +1,36 @@
+'''
+messagebox
+'''
+
+'''
+Programmatically Quit
+'''
+
+import sys
+from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+    
+    def initUI(self):
+        self.setGeometry(300,300,250,150)
+        self.setWindowTitle('Message Box')
+        self.show()
+    
+    def closeEvent(self, event): # Handle QCloseEvent
+        reply = QMessageBox.question(self, 'Message',
+        "Are yur sure to quit?", QMessageBox.Yes
+        | QMessageBox.No, QMessageBox.No )
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
