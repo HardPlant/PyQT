a7008023a95d4d1a3ec342f26ba5e4057d218957
HardPlant <abc7988se@naver.com>
2018-08-02 12:19:57 +0900

    read it

--- ptest_read.py
+++ ptest_read.py
@@ -1,6 +1,9 @@
 '''
 python profile : read this
 python -m cProfile -o output.txt ptest.py
+
+https://www.blog.pythonlibrary.org/2016/05/24/python-101-an-intro-to-benchmarking-your-code/
+http://www.blog.pythonlibrary.org/2014/03/20/python-102-how-to-profile-your-code/
 '''
 
 import pstats
